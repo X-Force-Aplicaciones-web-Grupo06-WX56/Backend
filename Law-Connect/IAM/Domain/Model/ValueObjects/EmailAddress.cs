19e7a80a8eb18d34354780f26144baed8e4d4dcd
asmip10 <longsamata47@gmail.com>
2024-11-09 20:53:54 -0500

    feat(IAM) : add user aggregate

---
+++ Law-Connect/IAM/Domain/Model/ValueObjects/EmailAddress.cs
@@ -0,0 +1,7 @@
+﻿namespace Law_Connect.IAM.Domain.Model.ValueObjects
+{
+    public record EmailAddress(string Address)
+    {
+        public EmailAddress() : this(string.Empty) { }
+    }
+}
---
+++ Law-Connect/IAM/Domain/Model/ValueObjects/PersonName.cs
@@ -0,0 +1,12 @@
+﻿namespace Law_Connect.IAM.Domain.Model.ValueObjects
+{
+    public record PersonName(string FirstName, string LastName)
+    {
+        public PersonName() : this(string.Empty,string.Empty) 
+        {
+        }
+        public PersonName(string FirstName) : this(FirstName, string.Empty) { }
+
+        public string FullName => $"{FirstName} {LastName}";
+    }
+}
